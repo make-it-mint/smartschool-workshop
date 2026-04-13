3abfb7ce606bcea4b9af219a99c71dd6b1f4b502
moppi <felix.riedel@make-it-mint.de>
2026-04-13 15:09:59 +0200

    fixed issues with the setup script. splitted it in to parts and added a reboot, beacuse the second part that uses docker needs a shell that is part of der docker groupd. Only a reboot enables this systemwide securely

--- rpi-files/smartschool/setup_system.sh
+++ rpi-files/smartschool/setup_system.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-# Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/debian
-Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-Components: stable
-Architectures: $(dpkg --print-architecture)
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-
-sudo apt update
-
-#Downloading and installing docker
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-#configuring os for docker
-sudo usermod -aG docker $USER
-newgrp docker
-
 
 #Setting up permissions
 sudo chown -R 1000:1000 ./node-red-data
@@ -52,3 +26,5 @@ EOF
     sudo systemctl restart NetworkManager
 	
 fi
+
+docker compose up -d
